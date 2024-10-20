5b96db2ade8fd7b5fc2e6c03610975ff1631bfae
Chavindu Nuwanpriya <chavindun@gmail.com>
2024-10-21 01:12:33 +0530

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -48,8 +48,8 @@ do
   dateTime=$(date +'%Y-%m-%d %H:%M')
   if [[ $commit == true ]]
   then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
+    echo "$dateTime, $result" >> "logs/${key}_report.log"
+    # By default we keep the last 2000 log entries. This will create the file if it doesn't exist.
     echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
     echo "    $dateTime, $result"
